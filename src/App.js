7c95b856a93438a14639b224272c3760d534f300
balaji <bbalaji13@gmail.com>
2023-02-27 19:19:52 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello World</h1>
+      <p>new</p>
+      <p>how r u</p>
+      <p>new world</p>
+      <p>naresh</p>
+      <p>new thing s on a field</p>
+      <p>ho ho ho</p>
+      <p>funny thing</p>
+      <p>hello</p>
+      <h1>sridhar</h1>
+    </div>
+  );
+}
+
+export default App;
